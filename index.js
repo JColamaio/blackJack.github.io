257d2c1c5306a58cb776dc12dbd2e99d3dc14154
JColamaio <jcolamaio@gmail.com>
2022-06-21 19:42:29 -0300

    add img

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? "
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "You've got Blackjack! "
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message ="You're out of the game! "
     isAlive = false
 }
+console.log(message);
