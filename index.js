f23600c171a0ac903aedab4febbb10750f64ef90
JColamaio <jcolamaio@gmail.com>
2022-06-21 22:25:06 -0300

    js functions 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard, secondCard] //array ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -12,8 +13,9 @@ function startGame() {
     renderGame();
 }
 function renderGame(){
+    cardsEl.textContent = "Card:" + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum:" + sum
-    cardsEl.textContent = "Card:" + firstCard + " " + secondCard
+    
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
@@ -29,6 +31,7 @@ function renderGame(){
 function newCard() {
     let card = 5;
     sum = sum + card
+    cards.push(card)
     renderGame()
 }
 
