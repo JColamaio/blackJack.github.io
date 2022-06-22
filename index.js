283a0e4625761f5ecf606c190ff7110d4274b163
JColamaio <jcolamaio@gmail.com>
2022-06-21 21:16:49 -0300

    js functions

--- index.js
+++ index.js
@@ -4,14 +4,33 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("messages")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cartas")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? "
-} else if (sum === 21) {
-    message = "You've got Blackjack! "
-    hasBlackJack = true
-} else {
-    message ="You're out of the game! "
-    isAlive = false
+function startGame() {
+    renderGame();
 }
-console.log(message);
+function renderGame(){
+    sumEl.textContent = "Sum:" + sum
+    cardsEl.textContent = "Card:" + firstCard + " " + secondCard
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        message = "You've got Blackjack! "
+        hasBlackJack = true
+    } else {
+        message ="You're out of the game! "
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = 5;
+    sum = sum + card
+    renderGame()
+}
+
+
+
